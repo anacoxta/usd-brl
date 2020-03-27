c62045efd176c0f3f637e3da32c68bbb8deda445
anacoxta <ana.costa@btwo.com.br>
2020-03-27 12:36:34 -0300

    Create Credits component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Container from '../src/Components/Container'
-import FormSection from './Components/FormSection';
-import Title from './Components/Title';
+import Container from "../src/Components/Container";
+import FormSection from "./Components/FormSection";
+import Title from "./Components/Title";
+import Credits from "./Components/Credits";
 
 const App = () => {
-
   return (
-    <Container>
-      <FormSection>
-        <Title />
-      </FormSection>
-    </Container>
-
-  )
-}
+    <>
+      <Container>
+        <FormSection>
+          <Title />
+        </FormSection>
+      </Container>
+      <Credits />
+    </>
+  );
+};
 
 export default App;
---
+++ src/Components/Credits.js
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "styled-components";
+
+// RENDER COMPONENT
+
+const Credits = props => {
+  return (
+    <StyledCredits>
+      <Text>react + material-ui + styled-components</Text>
+      <div className="divider"></div>
+      <Text>
+        ana costa eduardo • <a href="https://github.com/anacoxta">github</a> • <a href="https://glitch.com/@anacoxta">glitch</a> •{" "}
+        <a href="https://linkedin.com/in/anaeduardo">linkedin</a>
+      </Text>
+    </StyledCredits>
+  );
+};
+
+// STYLED-COMPONENT
+
+const StyledCredits = styled.div`
+  display: grid;
+  grid-template-rows: repeat(3, min-content);
+  max-width: 100%;
+  margin: 2rem;
+  text-align: center;
+  .divider {
+    width: 100%;
+    height: 1px;
+    background-color: white;
+    margin: 0.25rem 0;
+  }
+`;
+
+const Text = styled.p`
+  color: white;
+  font-weight: 600;
+  font-size: 0.8rem;
+  letter-spacing: 1px;
+  text-align: center;
+
+  a, a:visited {
+    text-decoration: none;
+    color: white;
+  }
+  a:hover {
+    color: darkblue;
+  }
+
+  @media (max-width: 645px) {
+    margin: 1rem;
+    & {
+      letter-spacing: 0;
+      font-weight: 400;
+    }
+  }
+
+  @media (max-width: 275px) {
+    & { font-size: 0.7rem; }
+  }
+`;
+
+export default Credits;
