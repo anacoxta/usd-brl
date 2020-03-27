1d90920cb19a3b1f0f8536a1e7514b8b76892a72
anacoxta <ana.costa@btwo.com.br>
2020-03-27 12:21:21 -0300

    Create global styles and overrides

--- src/index.js
+++ src/index.js
@@ -1,13 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core";
+import { createGlobalStyle } from "styled-components";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#5efc82",
+      main: "#00c853",
+      dark: "#009624",
+      contrastText: "#171c34",
+    },
+    secondary: {
+      light: "#62727b",
+      main: "#37474f",
+      dark: "#102027",
+      contrastText: "#fff",
+    },
+  },
+});
+
+const GlobalStyle = createGlobalStyle`
+  
+  * {
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    display: flex;
+    justify-content: center;
+    background-color: #0DB14B;
+    min-width: 100vw;
+    margin: 0;
+    padding: 0;
+    overflow-x: hidden;
+    font-family: 'Titillium Web', sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+  }
+  
+  #root {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
 
 ReactDOM.render(
-  <React.StrictMode>
+  <MuiThemeProvider theme={theme}>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>,
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
 
